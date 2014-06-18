bb571062be9c22d4bf7568d500319c9c057b9032
paulyi326 <paul.yi326@gmail.com>
2014-06-18 15:03:57 -0700

    Pass 3rd test

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -5,6 +5,10 @@ var PlayerView = Backbone.View.extend({
   // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
   el: '<audio controls autoplay />',
 
+  events: {
+    'ended': 'ended'
+  },
+
   initialize: function() {
   },
 
@@ -15,6 +19,10 @@ var PlayerView = Backbone.View.extend({
 
   render: function(){
     return this.$el.attr('src', this.model ? this.model.get('url') : '');
+  },
+
+  ended: function () {
+    this.model.trigger('ended');
   }
 
 });
