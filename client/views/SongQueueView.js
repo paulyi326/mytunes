8dd5e2200e8456cee43795947e7c23a7251eafff
paulyi326 <paul.yi326@gmail.com>
2014-06-18 16:17:49 -0700

    Pass SongQueueView tests

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -4,8 +4,24 @@ var SongQueueView = Backbone.View.extend({
   initialize: function() {
   },
 
-  render: function() {
-    return this.$el;
+  tagName: 'table',
+
+  initialize: function() {
+    this.collection.on('all', function () {
+      this.render();
+    }, this);
+  },
+
+  render: function(){
+    // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
+    // see http://api.jquery.com/detach/
+    this.$el.children().detach();
+
+    this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
 
 });
