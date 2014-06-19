ea4f9494b48638d8862035ddfa15f27cffc04bc1
paulyi326 <paul.yi326@gmail.com>
2014-06-18 18:38:35 -0700

    Refactor

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -6,11 +6,14 @@ var SongQueueEntryView = Backbone.View.extend({
   template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
 
   events: {
-    'click': function() {
-    }
+    'click': 'remove'
   },
 
   render: function(){
     return this.$el.html(this.template(this.model.attributes));
+  },
+
+  remove: function(){
+    this.model.dequeue();
   }
 });
